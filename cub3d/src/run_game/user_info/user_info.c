c59afeb076c7a1d19d0d926526325e718f4bb241
Hwano Jeon <hjeon@c2r6s7.42seoul.kr>
2020-06-08 13:21:08 +0900

    norm cub3d

--- cub3d/src/run_game/user_info/user_info.c
+++ cub3d/src/run_game/user_info/user_info.c
@@ -6,7 +6,7 @@
 /*   By: hjeon <hjeon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/04 17:15:53 by hjeon             #+#    #+#             */
-/*   Updated: 2020/06/08 11:16:39 by hjeon            ###   ########.fr       */
+/*   Updated: 2020/06/08 13:19:30 by hjeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,19 +32,19 @@ void	get_user_info(t_game_info *game_info, t_user_view *user_view)
 		while (++x < game_info->map_width)
 		{
 			if (game_info->map[y][x] == 'N' || game_info->map[y][x] == 'S' ||
-			 game_info->map[y][x] == 'W' || game_info->map[y][x] == 'E')
-			 {
+					game_info->map[y][x] == 'W' || game_info->map[y][x] == 'E')
+			{
 				user_view->pos_x = x + 0.5;
 				user_view->pos_y = y + 0.5;
 				if (game_info->map[y][x] == 'N')
-					set_dp(user_view, (double []){0, -1}, (double []){0.6, 0});
+					set_dp(user_view, (double[]){0, -1}, (double[]){0.6, 0});
 				else if (game_info->map[y][x] == 'S')
-					set_dp(user_view, (double []){0, 1}, (double []){-0.6, 0});
+					set_dp(user_view, (double[]){0, 1}, (double[]){-0.6, 0});
 				else if (game_info->map[y][x] == 'W')
-					set_dp(user_view, (double []){1, 0}, (double []){0, 0.6});
+					set_dp(user_view, (double[]){1, 0}, (double[]){0, 0.6});
 				else if (game_info->map[y][x] == 'E')
-					set_dp(user_view, (double []){-1, 0},(double []){0, -0.6});
-			 }
+					set_dp(user_view, (double[]){-1, 0}, (double[]){0, -0.6});
+			}
 		}
 	}
 }
